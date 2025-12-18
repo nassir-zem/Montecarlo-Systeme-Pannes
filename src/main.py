9e77f907e6b9785e6d96c57f9a7cd6ea17b9cb92
nassir-zem <etudeensa@gmail.com>
2025-12-18 20:57:14 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,8 @@
+from simulation import simulate_system
+from parameters import machines, T_TOTAL
 
+N = 10000
+results = [simulate_system(machines, T_TOTAL) for _ in range(N)]
+
+mean_availability = sum(results) / N
+print("Disponibilité moyenne simulée :", mean_availability)
